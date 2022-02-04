8fccf59d9ebee0a6516d699ef75cd85bc5fb6765
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-02-04 15:23:58 +0530

    updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,8 @@
 export default function Home() {
     return (
         <div>
-            <h1 style={{display:"flex", marginTop:"50px", justifyContent:"center", color:"white"}}>WELCOME</h1>
+            <h1 className="home-page">WELCOME TO CAMERA & PROFILE </h1>
+            <h1 style={{marginLeft:"170px"}}>APPLICATION</h1>
         </div>
     )
 };
